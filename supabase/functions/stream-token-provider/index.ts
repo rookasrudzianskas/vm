d4283aeb6e319d7e5c03ef728da43d9e2898716b
rokas <rookas.rudzenskas@gmail.com>
2024-12-10 13:42:46 +0100

    fix

--- supabase/functions/stream-token-provider/index.ts
+++ supabase/functions/stream-token-provider/index.ts
@@ -3,18 +3,37 @@
 // This enables autocomplete, go to definition, etc.
 
 // Setup type definitions for built-in Supabase Runtime APIs
-import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { StreamChat } from "npm:stream-chat";
+import { createClient } from 'jsr:@supabase/supabase-js@2'
 
-console.log("Hello from Functions!")
+console.log(Deno.env.get("STREAM_API_KEY"));
+
+const serverClient = StreamChat.getInstance(
+  Deno.env.get("STREAM_API_KEY"),
+  Deno.env.get("STREAM_API_SECRET")
+);
 
 Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
-  }
+  const authHeader = req.headers.get('Authorization')!
+  const supabaseClient = createClient(
+    Deno.env.get('SUPABASE_URL') ?? '',
+    Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+    { global: { headers: { Authorization: authHeader } } }
+  )
+
+  const authHeader = req.headers.get('Authorization')!
+  const token = authHeader.replace('Bearer ', '')
+  const { data } = await supabaseClient.auth.getUser(token)
+  const user = data.user
+
+  console.log(user);
+
+  const token = serverClient.createToken("john");
+  console.log(token);
 
   return new Response(
-    JSON.stringify(data),
+    JSON.stringify({ token }),
     { headers: { "Content-Type": "application/json" } },
   )
 })
