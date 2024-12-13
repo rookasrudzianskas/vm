23498d17d7b94c42131ca2bccf084760761b58d7
rokas <rookas.rudzenskas@gmail.com>
2024-12-13 07:38:52 +0100

    Fix

--- src/app/(protected)/(tabs)/chat.tsx
+++ src/app/(protected)/(tabs)/chat.tsx
@@ -1,11 +1,25 @@
 import { Stack, Link } from 'expo-router';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { Button, SafeAreaView, View } from 'react-native';
 import { Channel as ChannelType } from 'stream-chat';
-import { Channel, ChannelList, MessageInput, MessageList } from 'stream-chat-expo';
+import { Channel, ChannelList, MessageInput, MessageList, useChatContext } from "stream-chat-expo";
+import { useAuth } from "~/src/contexts/AuthProvider";
 
 export default function Home() {
   const [channel, setChannel] = useState<ChannelType>();
+  const { client } = useChatContext();
+  const { user } = useAuth();
+
+  const filters = {
+    members: { $in: [user?.id] },
+  }
+
+  useEffect(() => {
+    const channel = client.channel("messaging", "abssss", {
+      name: "The Park",
+    });
+    channel.watch();
+  }, []);
 
   return (
     <View className="flex-1 bg-white pt-16">
