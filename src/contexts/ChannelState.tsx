7f186512715a5c7971261e420b0e2969e8977285
rokas <rookas.rudzenskas@gmail.com>
2024-12-13 11:16:55 +0100

    fix

--- src/contexts/ChannelState.tsx
+++ src/contexts/ChannelState.tsx
@@ -1,9 +1,28 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useAuth } from "./AuthProvider";
+import { useChatContext } from "stream-chat-expo";
 
 const ChannelContext = createContext<any>(undefined);
 
 export function ChannelStateProvider({ children }: { children: React.ReactNode }) {
   const [channel, setChannel] = useState<any>(undefined);
+  const { user } = useAuth();
+  const { client } = useChatContext();
+
+  useEffect(() => {
+    const joinDefaultChannel = async () => {
+      const defaultChannel = client.channel("messaging", `${user?.id}-default`, {
+        name: channel?.title,
+        members: [user?.id],
+      })
+        .watch();
+      await channel.watch();
+      setChannel(defaultChannel);
+    }
+    joinDefaultChannel().then(r => {
+      console.log("Joined default channel");
+    });
+  }, [user]);
 
   return (
     <ChannelContext.Provider value={{ channel, setChannel }}>
