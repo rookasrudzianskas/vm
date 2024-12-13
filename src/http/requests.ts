6df15b4dc8fb17e0d9ffccc8423420d55c1ce71e
rokas <rookas.rudzenskas@gmail.com>
2024-12-13 08:26:00 +0100

    fix

--- src/http/requests.ts
+++ src/http/requests.ts
@@ -1,7 +1,7 @@
 import { post } from "~/src/http/api";
 
 export const startAI = async (channelId: string) => {
-  post('http://localhost:3000/start-ai-agent', { channel_id: channelId });
+  post('http://localhost:3000/start-ai-agent', { channel_id: channelId, platform: 'openai' });
 };
 export const stopAI = async (channelId: string) => {
   post('http://localhost:3000/stop-ai-agent', { channel_id: channelId });
